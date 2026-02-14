045cd6c747faece004803c112a8d0d5e3e687250
layanzaferr <lez6349@rit.edu>
2026-02-15 00:15:01 +0400

    Created Spring Boot application and added first bean

---
+++ EventHub/src/main/java/edu/rit/eventhub/AppController.java
@@ -0,0 +1,13 @@
+package edu.rit.eventhub;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+    @RequestMapping("/")
+    public String getLoginPage(){
+        return "index.html";
+    }
+}
---
+++ EventHub/src/main/java/edu/rit/eventhub/model/User.java
@@ -0,0 +1,43 @@
+package edu.rit.eventhub.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    private String userName;
+    private String email;
+    private String password;
+    private int userId;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
